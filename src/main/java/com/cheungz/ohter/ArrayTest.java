e1f77bdff687de97751c72f7bd89d149825ea1f3
z'z <zhangzhe201507@foxmail.com>
2021-01-24 23:48:55 +0800

    Design Pattern Learning

--- src/main/java/com/cheungz/ohter/ArrayTest.java
+++ src/main/java/com/cheungz/ohter/ArrayTest.java
@@ -1,9 +1,5 @@
 package com.cheungz.ohter;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-
 
 /**
  * @program: More
@@ -13,25 +9,9 @@ import java.util.Map;
  **/
 public class ArrayTest {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
 
-        /*Map<String,String> map = new HashMap<>();
-        map.put("String","String");
-        map.put("map","map");
-        *//*for (java.util.Map.Entry<String, String> e : map.entrySet()) {
-            System.out.println(e.getKey());
-            System.out.println(e.getValue());
-            System.out.println(map.keySet());
-        }*//*
-        for(String name : map.keySet()){
-            System.out.println(name);
-        }*/
-
-        String str3="aaa";
-        String str4="aaa";
-        System.out.println(str3==str4);
-
-
+        System.out.println("git分支测试");
 
     }
 }
