53b990e76b21d35377c4984449f8d76b93bf4b4f
cheungz <zhangzhe201507@foxmail.com>
2021-04-25 22:15:05 +0800

    设计模式学习

--- src/main/java/com/cheungz/other/MapEntryTest.java
+++ src/main/java/com/cheungz/other/MapEntryTest.java
@@ -15,13 +15,5 @@ public class MapEntryTest {
         Map<String,Integer> map = new HashMap();
         map.put("张三",18);
         map.put("李四",19);
-        /*Set set = map.keySet();
-        Iterator iterator = set.iterator();
-        while (iterator.hasNext())
-            System.out.println(iterator.next()+" = "+map.get(iterator.next()));*/
-
-
-        for (Map.Entry<String, Integer> entry : map.entrySet())
-            System.out.println(entry.getKey()+" = "+entry.getValue());
     }
 }
